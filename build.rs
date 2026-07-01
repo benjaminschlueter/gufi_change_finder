e17d7ece9780a6c33f885a8a62bb0cb8bf0c993f
Thomas Bertschinger <bertschinger@lanl.gov>
2026-07-01 12:25:36 -0600

    build: address warnings and standardize process of generating bindings
    When compiled under Rust 2024, the bindings produce warnings about
    unsafe operations in unsafe functions. See
    
      https://github.com/rust-lang/rust-bindgen/issues/3147
    
    for a discussion about how this applies to bindgen.
    
    To resolve some of the warnings, this adds
    
      .wrap_unsafe_ops(true)
    
    to the build script.
    
    However, that doesn't appear to fix every single use of an unsafe
    operation that causes a warning. To resolve the remaining warnings, this
    wraps the bindings module in src/lib.rs with
    
      #[allow(unsafe_op_in_unsafe_fn)]
    
    The generation of bindings was also non-standard; there's a standard way
    that generated code is handled in Rust, which is to have the code
    generated in the target/ directory and to include the code into a module
    in the library. Rather than doing that, the build was putting the
    generated code into the src/ directory and then including it directly
    into src/lib.rs.
    
    This also removes some redundant work in the build process,
    specifically, generating the bindings into a tmp file and then copying
    them over into the final file.
    
    Finally, this addresses other minor warnings in build.rs.

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
-use std::{fs, io};
 use std::path::PathBuf;
 use std::fs::File;
 use std::io::Write;
-use std::io::ErrorKind;
 use std::process::Command;
 
 fn main() {
@@ -13,33 +11,21 @@ fn main() {
     log_file.write_all(b"Top level MarFS source directory: ").expect("Failed to write to build.log");
     log_file.write_all(b"\n").expect("Failed to write to build.log");
 
-    let scoutwrap_clean = Command::new("make")
-                            .arg("-C")
-                            .arg("src/scoutwrap")
-                            .arg("clean")
-                            .status()
-                            .expect("failed to clean src/scoutwrap");
+    Command::new("make")
+        .arg("-C")
+        .arg("src/scoutwrap")
+        .arg("clean")
+        .status()
+        .expect("failed to clean src/scoutwrap");
 
-    let scoutwrap_make = Command::new("make")
-                            .arg("-C")
-                            .arg("src/scoutwrap")
-                            .status()
-                            .expect("failed to make src/scoutwrap");
+    Command::new("make")
+        .arg("-C")
+        .arg("src/scoutwrap")
+        .status()
+        .expect("failed to make src/scoutwrap");
 
        
-    let bindings_path = "src/bindings.rs";
-    match fs::remove_file(bindings_path) {
-        Ok(_) => { },
-        Err(e) => {
-            if e.kind() == ErrorKind::NotFound {
-                log_file.write_all(b"\nfailed to remove old bindings file: does not exist\n").expect("failed to write to build.log");
-            }
-            else {
-                log_file.write_all(b"\nfailed to remove old bindings file\n").expect("failed to write to build.log");
-            }
-        }
-    };
-
+    let bindings_path = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("bindings.rs");
 
     // ScoutFS
     let scoutfs_path = PathBuf::from("src/scoutfs").canonicalize().expect("Cannot canonicalize path");
@@ -63,24 +49,11 @@ fn main() {
                     .clang_arg(format!("-I{}/src/scoutfs", top_path.to_str().unwrap()))
                     .clang_arg("-I/usr/include/libxml2")
                     .blocklist_item("^FP_.*$") // for some reason, FP_NAN, etc. are defined twice, so block them and use the libc variant
+                    .wrap_unsafe_ops(true)
                     .generate()
                     .expect("Failed to generate bindings for {header_path_str");
     
-    bindings.write_to_file("src/bindings.tmp").expect("Failed to write to bindings.tmp");
-    let mut tmp_bindings = fs::OpenOptions::new()
-            .read(true)
-            .open("src/bindings.tmp")
-            .unwrap();
-
-    let mut real_bindings = fs::OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open(bindings_path)
-            .unwrap();
-
-    let _result = io::copy(&mut tmp_bindings, &mut real_bindings);
-
-    fs::remove_file("src/bindings.tmp").expect("Failed to remove src/bindings.tmp");
+    bindings.write_to_file(bindings_path).expect("Failed to write to bindings.tmp");
 
     //let log_string = format!("Wrote bindings from {header_path_str} to {bindings_path}\n"); 
     //log_file.write_all(log_string.as_bytes()).expect("Failed to write to build.log");
@@ -91,36 +64,4 @@ fn main() {
     println!("cargo:rustc-env=LD_LIBRARY_PATH={}", scoutwrap_lib_path.to_str().unwrap());
     println!("cargo:rustc-link-lib=scoutwrap");
 
-}
-
-fn include_header(header_path: &str, bindings_path: &str, log_file: &mut File, top_path: &str) {
-
-
-    let bindings = bindgen::Builder::default()
-                    .header(header_path)
-                    .clang_arg(format!("-I{}/src/scoutfs", top_path))
-                    .clang_arg("-I/usr/include/libxml2")
-                    .generate()
-                    .expect("Failed to generate bindings for {header_path_str");
-    
-    bindings.write_to_file("src/bindings.tmp").expect("Failed to write to bindings.tmp");
-    let mut tmp_bindings = fs::OpenOptions::new()
-            .read(true)
-            .open("src/bindings.tmp")
-            .unwrap();
-
-    let mut real_bindings = fs::OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open(bindings_path)
-            .unwrap();
-
-    let _result = io::copy(&mut tmp_bindings, &mut real_bindings);
-
-    fs::remove_file("src/bindings.tmp").expect("Failed to remove src/bindings.tmp");
-
-    let log_string = format!("Wrote bindings from {header_path} to {bindings_path}\n"); 
-    log_file.write_all(log_string.as_bytes()).expect("Failed to write to build.log");
-
-    
 }
--- src/scoutwrap.rs
+++ src/scoutwrap.rs
@@ -1,4 +1,4 @@
-use gufi_change_finder::*;
+use gufi_change_finder::bindings::*;
 
 use std::fs::File;
 use std::os::fd::AsRawFd;
