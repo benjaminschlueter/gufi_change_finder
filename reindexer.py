85620692eeb3538cebaa0cfdd1b30b30aafd8092
Benjamin Schlueter <bschlueter@lanl.gov>
2026-09-21 16:56:04 -0600

    fixed treesummary error check bug

--- reindexer.py
+++ reindexer.py
@@ -75,7 +75,7 @@ for path in paths:
     parent_fuse_path = "/".join(parent_fuse_path_split)
     
     # call gufi_dir2index with MarFS plugin
-    result = subprocess.run([f"{GUFI_PATH}/src/gufi_dir2index", "-x", "--threads", str(THREAD_COUNT),  "--plugin", f"GUFI_MARFS_PLUGIN:{GUFI_PATH}/contrib/plugins/libmarfs_plugin.so", path[0], f"{WORK_REINDEX_DIR}{parent_fuse_path}"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True) 
+    result = subprocess.run([f"{GUFI_PATH}/src/gufi_dir2index", "-x", "--threads", str(THREAD_COUNT),  "--plugin", f"GUFI_MARFS_PLUGIN:{GUFI_PATH}/contrib/plugins/libmarfs_plugin.so", path[0], f"{WORK_REINDEX_DIR}{parent_fuse_path}"], stdout=subprocess.DEVNULL, check=True) 
 
 
 for path in paths:
@@ -125,9 +125,12 @@ result = subprocess.run(["mkdir", "-p", WORK_REINDEX_DIR], check=True)
 print(f"Regenerating treesummaries")
 result = subprocess.run([f"{GUFI_PATH}/src/gufi_treesummary_all", GUFI_INDEX_DIR], stdout=subprocess.DEVNULL)
 
-if result.returncode == 1 and not Path(f"{GUFI_INDEX_DIR}/db.db").is_file():
-    print("Error: GUFI tree root contains no db.db")
-    print("The reindexer requires an existing GUFI tree before performing incrementals")
+if result.returncode == 1: 
+    if not Path(f"{GUFI_INDEX_DIR}/db.db").is_file():
+        print("Error: GUFI tree root contains no db.db")
+        print("The reindexer requires an existing GUFI tree before performing incrementals")
+    
+    sys.exit(1)
 
 # write validation file 
 if VALIDATE_STATE:
